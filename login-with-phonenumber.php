582e20aa2b8057906a7d19827e58531324b8ed04
Hamid Alinia <a.goodlookingboy@gmail.com>
2024-06-09 16:22:38 +0330

    Short Description

--- login-with-phonenumber.php
+++ login-with-phonenumber.php
@@ -3,7 +3,8 @@
 Plugin Name: Login with phone number
 Plugin URI: https://loginwithphonenumber.site
 Description: Login with phone number - sending sms - activate user by phone number - limit pages to login - register and login with ajax - modal
-Version: 1.7.30
+Short Description: Login with mobile number in WordPress
+Version: 1.7.31
 Author: Hamid Alinia - idehweb
 Author URI: https://loginwithphonenumber.site
 Text Domain: login-with-phone-number

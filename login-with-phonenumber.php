7ac682d4042c1da8333b030478863d0c88c80ce7
ehsanunity2079 <ehsanunity2079@gmail.com>
2025-04-13 09:29:55 +0330

    change iti__select-flag to css file ( !important)

--- login-with-phonenumber.php
+++ login-with-phonenumber.php
@@ -3,7 +3,7 @@
 Plugin Name: Login with phone number
 Plugin URI: https://idehweb.com
 Description: Login with phone number - sending sms - activate user by phone number - limit pages to login - register and login with ajax - modal
-Version: 1.7.94
+Version: 1.7.95
 Author: Hamid Alinia - idehweb
 Author URI: https://idehweb.com
 Text Domain: login-with-phone-number

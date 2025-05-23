c4dd567ed98abcc75678e6df39a2d77d6c455e73
Hamid Alinia <a.goodlookingboy@gmail.com>
2025-05-23 14:38:43 +0330

    update iti flags

--- login-with-phonenumber.php
+++ login-with-phonenumber.php
@@ -3,7 +3,7 @@
 Plugin Name: Login with phone number
 Plugin URI: https://idehweb.com/product/login-with-phone-number-in-wordpress/
 Description: Login with phone number - sending sms - activate user by phone number - limit pages to login - register and login with ajax - modal
-Version: 1.8.12
+Version: 1.8.13
 Author: Hamid Alinia - idehweb
 Author URI: https://idehweb.com/product/login-with-phone-number-in-wordpress/
 Text Domain: login-with-phone-number
